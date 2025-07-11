e8347b48b453d85a8fc0048cf92a3cf9c97e45e6
Tgthegood <almeidaaxel52@gmail.com>
2025-07-11 20:24:32 +0200

    Add FanMtl function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ let { connect } = require("puppeteer-real-browser");
 let path = require("path")
 let fs = require('fs');
 
+
+function support_message() {
+  console.log(`
+  ⭐ If you find this module useful, consider supporting me! ⭐
+  ☕ Buy Me a Coffee: https://buymeacoffee.com/almeidaaxep  
+  💖 Patreon:         https://patreon.com/Tgthegood52
+  `);
+}
+
 function default_path(){
     return __dirname;
 }
@@ -15,7 +24,7 @@ function path_sanitize(inputPath) {
 let sanitized = inputPath.replace(/\//g, '/');
   sanitized = path.normalize(sanitized);
   if (!fs.existsSync(sanitized)) {
-    throw new Error(`The path does not Exist: ${sanitized}`);
+    throw new Error(`The path does not Exist: ${sanitized}, Example C:/user/pathToNovel`);
   }
   return sanitized;
 }
@@ -63,6 +72,7 @@ async function webnovel_scroll(page, parrafosSelector, paragraph_loading) {
 
 //Init functions for novel scrapping
 async function webnovel(link=false, path_novel=default_path()) {
+    support_message()
     path_sanitize(path_novel)
     if(!link){throw new Error("You need to provide a valid Webnovel link")}
 
@@ -79,7 +89,12 @@ async function webnovel(link=false, path_novel=default_path()) {
     await page.goto(link, {waitUntil:"load"});
     let name_novel = await page.evaluate(() => {
         let el = document.querySelector(".cha-page-in h2");
-        return el ? el.textContent.trim().replace(/[^a-zA-Z\s]/g, '') : "Title was not Found";
+        return el
+            ? el.textContent
+                .trim()
+                .replace(/[*?"<>|:/\\]/g, '')
+                .replace(/[\u0000-\u001F]/g, '')
+            : "Title was not Found";
     });
 
     //Selectors in the novel
@@ -110,10 +125,10 @@ async function webnovel(link=false, path_novel=default_path()) {
     }
 
     console.log("\n\nWaiting for confirmation...");
-    let final_path = path.join(path_novel, name_novel);
+    let final_path = path.join(path_novel, `${name_novel}.txt`);
     try {
     fs.writeFileSync(final_path, content_webnovel, "utf-8");
-    console.log(`${name_novel} was correctly scanned`);
+    console.log(`${name_novel} was correctly scanned on ${final_path}`);
         await browser.close();
     } catch (error) {
     console.error(`${name_novel} failed, reason not found`);
@@ -121,6 +136,102 @@ async function webnovel(link=false, path_novel=default_path()) {
     }
 };
 
+
+//Init FanMtl functions
+async function fanmtl(link=false, path_novel=default_path(), ublock=false) {
+    support_message()
+    path_sanitize(path_novel)
+    if(!link){throw new Error("You need to provide a valid FanMtl link")}
+    if(!link.includes("1.html")){throw new Error("Youn need to provide the link of the Chapter 1")}
+    if(!ublock){throw new Error("You need to provide a valid Ublock Origin Path, Example C:/user/pathUblock")}
+
+    const { browser, page } = await connect({
+        headless: false,
+        args: [
+            '--disable-extensions-except=' + ublock,
+            '--load-extension=' + ublock,
+        ],
+        customConfig: {},
+        turnstile: true,
+        connectOption: {},
+        disableXvfb: false,
+        ignoreAllFlags: false,
+    });
+
+    await page.goto(link, {waitUntil:"load"});
+
+    let name_novel = await page.evaluate(() => {
+        let el = document.querySelector("div.content-wrap div.titles h1 a");
+        return el
+            ? el.getAttribute("title")
+                .trim()
+                .replace(/[*?"<>|:/\\]/g, '')
+            : "Title was not Found";
+    });
+
+    let content_fanmtl = ""
+    let i = 1
+    while(true){
+        let chapter_selector = await page.$(".chapter-content");
+
+        if(chapter_selector){
+            let chapter_content = await page.evaluate(() => {
+                let content = document.querySelector(".chapter-content");
+                if (!content) return '';
+
+                let chapter_trash = content.querySelectorAll("script, div, iframe, .ob-smartfeed-wrapper");
+                chapter_trash.forEach(a => a.remove());
+
+                let chapter_elements = Array.from(content.querySelectorAll("p"));
+                let chapter_sanitize = chapter_elements.filter(el => el.textContent.trim().length > 0);
+
+                if (chapter_sanitize.length > 0) {
+                    let chapter_set = new Set(chapter_sanitize.map(el => el.textContent.trim()));
+                    return Array.from(chapter_set).join("\n");
+                } else {
+                    let chapter_alternative = content.innerHTML.replace(/<br\s*\/?>/gi, '\n').trim();
+                    return chapter_alternative;
+                }
+            });
+            content_fanmtl += chapter_content+'\n';
+            process.stdout.write(`\rChapter: ${i} Obtained`);
+            i+=1
+
+            let chapter_next_button = await page.$(".nextchap");
+                if (chapter_next_button) {
+                    let click_succes = false;
+                    for (let attempt = 0; attempt < 5; attempt++) {
+                        try {
+                            await Promise.all([
+                                chapter_next_button.click(),
+                                page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 20000 })
+                            ]);
+                            await sleep_seconds(1)
+                            click_succes = true;
+                            break;
+                        } catch (error) {}
+                    }
+                    if (!click_succes) {
+                        break;
+                    }
+                } else {break}
+        }
+    }
+
+    console.log("\n\nWaiting for confirmation...");
+    let final_path = path.join(path_novel, `${name_novel}.txt`);
+    try {
+        fs.writeFileSync(final_path, content_fanmtl, "utf-8");
+        console.log(`${name_novel} was correctly scanned on ${final_path}`);
+        await browser.close();
+    } catch (error) {
+        console.error(`${name_novel} failed, reason not found`);
+        await browser.close();
+    }
+}
+
+
 module.exports = {
-    webnovel
+    webnovel,
+    fanmtl,
 };
--- test.js
+++ test.js
@@ -1,6 +1,15 @@
-let { webnovel } = require("./src/index.js")
+let { webnovel, fanmtl } = require("./src/index.js")
 
-webnovel("https://www.webnovel.com/book/pieces-of-the-past_32168295508965905/new-world_86374925098613666", "C:\Users\Tgthegood\Documents\Novels").then(() => {
+// webnovel("https://www.webnovel.com/book/pieces-of-the-past_32168295508965905/new-world_86374925098613666", "C:/Users/Tgthegood/Documents/Novels").then(() => {
+//   console.log("Descarga completada");
+// }).catch(err => {
+//   console.error("Error:", err);
+// });
+
+fanmtl("https://www.fanmtl.com/novel/6984700_1.html", 
+  "C:/Users/Tgthegood/Documents/Novels",
+  "C:/Users/Tgthegood/Documents/Ublock")
+  .then(() => {
   console.log("Descarga completada");
 }).catch(err => {
   console.error("Error:", err);
